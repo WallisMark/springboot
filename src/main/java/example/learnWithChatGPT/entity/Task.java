4a174f3999a77a6c94e8b05245c1f2780cfd9ded
Wallis Mark <wallis@gmail.com>
2023-09-05 11:52:28 +0200

    add entity and build.gradle file

--- src/main/java/example/learnWithChatGPT/entity/Task.java
+++ src/main/java/example/learnWithChatGPT/entity/Task.java
@@ -1,4 +1,18 @@
 package example.learnWithChatGPT.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
 public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+     private String name;
+     private String description;
 }
